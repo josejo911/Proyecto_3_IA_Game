a794d175985420b341e416e4ed5f1a8769e2df77
Jose Jo Escobar <jo14343@uvg.edu.gt>
2019-06-03 20:55:27 -0600

    Final de los tiempos

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Proyecto_3_IA_Game
+Final Project for Artificial Intelligence
+
+Othello Client implemented with minimax algorithm and poda alpha beta.
+
+Must be run with https://github.com/samuelchvez/open-1v1-board-game-coordinator
+
+npm run start
+--tid=<Tournament ID> 
+--user=<User ID> 
+--ip=<IP where the board game coordinator is running> 
+--port=<Port where the board game coordinator is running>
--- index.js
+++ index.js
@@ -1,165 +1,117 @@
-/**
- * JOSE JAVIER JO ESCOBAR
- * 14343
- * PROYECTO 3 IA -- OTHELLO GAME
- */
+const argv = require('yargs').argv
 
+var usern = 'JavierJo_Dimelopapiiiii'
+var puerto = '4000'
+var tID = '142857'
+var socket = require('socket.io-client')("http://192.168.1.148:" + puerto + "");  // for example: http://127.0.0.1:3000
 
-/**
- * Definicion de variables
- */
-var tournamentID=142857;
-var user_name='JavierJ_Chinitoveloxxx';
-var gameID ;
-var playerTurnID ;
-var myID;
-var rivalID;
-var bandera = false ;
-var winnerTurnID ;
-var movement =7;
-var move ;
-var board =[];
-var board2d =[];
-var lastMove;
-var __listaTiros= [];
+const b = require('./lib/board')
+const m = require('./lib/minimax')
 
-var socket = require('socket.io-client')('http://192.168.88.252:4000');
+let root = {}, bestChild = {}, countOfMoves = 0
+
+const rand_int = (min, max) => Math.floor(Math.random() * (max - min)) + min
+
 socket.on('connect', function(){
+  console.log("Signing in as", usern)
+
   socket.emit('signin', {
-    user_name: user_name,
-    tournament_id: tournamentID,
-    user_role: 'player'
-  });
-});
+    user_name: usern,
+    tournament_id: tID ,
+    user_role: 'player',
+  })
 
-socket.on('ok_signin', function(){
-  console.log("Ya estamos conectados!");
-});
+  socket.on('ok_signin', function(){
+    console.log("Successfully signed in!")
+  })
 
+  socket.on('ready', function(data){
+    const gameID = data.game_id
+    const playerTurnID = data.player_turn_id
+    const boardArray = data.board
+    const boardMatrix = b.transformBoard(boardArray)
+    const enemey_id = playerTurnID === 1 ? 2 : 1
 
-socket.on('ready', function(data){
-  var gameID = data.game_id;
-  var playerTurnID = data.player_turn_id;
-  myID=playerTurnID;
-  rivalID=rival(myID);
-  var board = data.board;
-
-  board2d=transformBoard(board);
-  var v= __valMax(0,board2d,7,-1000,1000);
-  move = get1DPos (__listaTiros[0],__listaTiros[1]);
-
-  if (move==lastMove){
-    move =Math.floor((Math.random() * 63) + 0);
-  }
-  lastMove=move ;
-  console.log(board2d);
-  socket.emit('play', {
-    tournament_id: tournamentID,
-    player_turn_id: playerTurnID,
-    game_id: gameID,
-    movement: move
-  });
-});
-
-socket.on('finish', function(data){
-  var gameID = data.game_id;
-  var playerTurnID = data.player_turn_id;
-  var winnerTurnID = data.winner_turn_id;
-  var board = data.board;
-
-  socket.emit('player_ready', {
-    tournament_id: tournamentID,
-    player_turn_id: playerTurnID,
-    game_id: gameID
-  });
-});
-
-/**
- * Definicion de Minimax
- * @param {*} vueltas 
- * @param {*} __actualizacionTablero1 
- * @param {*} depth 
- * @param {*} alpha 
- * @param {*} beta 
- */
-function __valMax(vueltas,__actualizacionTablero1,depth,alpha,beta){
-  let __actualizacionTablero= [];
-  for (var i=0; i<8;i++){
-      __actualizacionTablero.push(__actualizacionTablero1[i]);
-  }
-  var __cantVueltas = vueltas+1;
-  var v = -1000;
-  var tirosValidos = getTirosValidos(__actualizacionTablero,myID);
-
-  if (tirosValidos.length==0) {
-    v = finalValue(__actualizacionTablero);
-  } else {
-    for (var i=0;i<tirosValidos.length;i++) {
-      let tiroBoard=getBoard(tirosValidos[i],__actualizacionTablero,myID);
-      let tiroValue= value(__cantVueltas,-1,tiroBoard,depth,alpha,beta);
-      v = Math.max(v,tiroValue);
-      if (v==tiroValue){
-        __listaTiros=tirosValidos[i];
+    // Si es la primera vez pregeneramos el root
+    if(Object.keys(root).length === 0 && root.constructor === Object){
+      root = {
+        board: boardMatrix,
+        score: 0,
+        position: -1,
+        childs: []
       }
-      if (v>=beta){
-        return v;
+    } else {
+      // Encontramos el nuevo rood del mejor hijo
+      let newRootIndex = m.foundNewRootIndex(bestChild, boardMatrix)
+
+      root = bestChild.childs[newRootIndex]
+      if(root === undefined){
+        root = {
+          board: boardMatrix,
+          score: 0,
+          position: -1,
+          childs: []
+        }
       }
-      alpha = Math.max(alpha,v);
     }
-  }
-  return v;
-}
 
-function __valMin(vueltas,__actualizacionTablero1,depth,alpha,beta) {
-  let __actualizacionTablero= [];
-  for (var i=0; i<8;i++){
-      __actualizacionTablero.push(__actualizacionTablero1[i]);
-  }
-  var __cantVueltas = vueltas+1;
-  var v = 1000;
-  var tirosValidos = getTirosValidos(__actualizacionTablero,rivalID);
+    countOfMoves = b.leftCountOfMoves(boardMatrix)
 
-  if (tirosValidos.length==0){
-    v=finalValue(__actualizacionTablero);
-  } else {
-    for (var i=0;i<tirosValidos.length;i++) {
-      let tiroBoard=getBoard(tirosValidos[i],__actualizacionTablero,rivalID);
-      v = Math.min(v,value(__cantVueltas,1,tiroBoard,depth,alpha,beta));
+    let bestScore = 0
 
-      if (v<=alpha) {
-        return v;
-      }
-      beta = Math.min(beta,v);
+    if(countOfMoves <= 14){
+      bestScore = m.minimax(
+        root,
+        countOfMoves,
+        true,
+        playerTurnID,
+        enemey_id,
+        Number.NEGATIVE_INFINITY,
+        Number.POSITIVE_INFINITY,
+        countOfMoves
+      )
+    } else {
+      bestScore = m.minimax(
+        root,
+        6,
+        true,
+        playerTurnID,
+        enemey_id,
+        Number.NEGATIVE_INFINITY,
+        Number.POSITIVE_INFINITY,
+        countOfMoves
+      )
     }
-  }
-  return v;
-}
+
+    bestChild = root.childs[root.childs.findIndex(child => child.score === bestScore)]
+
+    let move = bestChild.position
+    console.log('move', move)
+
+    const play = {
+      tournament_id: tID,
+      player_turn_id: playerTurnID,
+      game_id: gameID,
+      movement: move,
+    }
+
+    socket.emit('play', play)
+  })
+
+  socket.on('finish', function(data){
+    console.log('finish')
+    const gameID = data.game_id
+    const playerTurnID = data.player_turn_id
+    const winnerTurnID = data.winner_turn_id
+    const board = data.board
 
 
-function value(vueltas,type,__actualizacionTablero1,depth,alpha,beta){
-  let __actualizacionTablero= [];
-  for (var i=0; i<8;i++){
-    __actualizacionTablero.push(__actualizacionTablero1[i]);
-  }
+    const player_ready = {
+      tournament_id: tID,
+      player_turn_id: playerTurnID,
+      game_id: gameID
+    }
 
-  if (vueltas>depth && type==-1 ) {
-    bandera=true ;
-    return Utility(__actualizacionTablero);
-  } else if (type==1) {
-    return __valMax(vueltas,__actualizacionTablero,depth,alpha,beta);
-  } else if (type==-1) {
-    return __valMin(vueltas,__actualizacionTablero,depth,alpha,beta);
-  }
-
-  return "error";
-}
-
-function Utility(__actualizacionTablero){
-  var flips = flipHeuritic(__actualizacionTablero,myID);
-  var movility = -1 *getTirosValidos(__actualizacionTablero,rival);
-  var difs = difInBoard(__actualizacionTablero);
-
-  var ww = __pesos(__actualizacionTablero);
-
-  return ww+movility+flips;
-}
+    socket.emit('player_ready', player_ready)
+  })
+})
